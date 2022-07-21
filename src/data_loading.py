32c45c5a0c585f22f8bd9b2db06d83dc125b37fb
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-07-21 15:08:33 +0200

    refactor, documentation about ignored dataset elements

--- src/data_loading.py
+++ src/data_loading.py
@@ -38,6 +38,7 @@ def load_dataset():
                         file4
                 ...
         The files must be CSV files containing a matrix of values separated by commas.
+        Matrices must have a 512 x 512 shape. Files containing a matrix with a different shape are ignored.
 
         Loaded data is saved in a cache directory. Subsequent calls to this function will ignore the dataset and load
         from the cache directory, to improve speed.
@@ -52,7 +53,7 @@ def load_dataset():
     else:
         images, masks = _read_dataset_from_disk()
         CACHE_FILE_PATH.parent.mkdir(exist_ok=True)
-        data_to_save = np.array([images, masks], dtype=object)
+        data_to_save = np.stack([images, masks])
         np.save(CACHE_FILE_PATH, data_to_save)
 
     return images, masks
@@ -99,13 +100,27 @@ def _read_dataset_from_disk():
             yield DatasetElement(subdirectory_path, file_path.name)
 
     dataset_elements = list(itertools.chain(*map(dataset_elements_from_subdirectory, dataset_subdirectory_paths)))
+
     images_paths = map(DatasetElement.image_path_string, dataset_elements)
     masks_paths = map(DatasetElement.mask_path_string, dataset_elements)
 
     def file_reader(csv_file_path: str):
         return np.loadtxt(csv_file_path, delimiter=",")
 
-    images = map(file_reader, images_paths)
-    masks = map(file_reader, masks_paths)
+    def dataset_element_files_reader(image_path:str, mask_path:str):
+        return file_reader(image_path), file_reader(mask_path)
 
-    return list(images), list(masks)
+    dataset_data = map(dataset_element_files_reader, images_paths, masks_paths)
+
+    def dataset_element_has_correct_shape(image_mask_tuple):
+        image, mask = image_mask_tuple
+        shape_is_correct = image.shape == (512, 512) and mask.shape == (512, 512)
+        if not shape_is_correct:
+            print("A dataset element was ignored because its shape is not 512 x 512")
+        return shape_is_correct
+
+    filtered_dataset_data = filter(dataset_element_has_correct_shape, dataset_data)
+
+    images, masks = zip(*filtered_dataset_data)
+
+    return np.array(images), np.array(masks)
