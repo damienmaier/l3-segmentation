d3b48d3fb52c055887f74d8d755d5f185f9fb446
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-07-22 17:47:32 +0200

    method to do prediction from model

---
+++ src/predict.py
@@ -0,0 +1,19 @@
+import keras.models
+import numpy as np
+
+import temp.cusom_objects_joachim
+from model_training import MODEL_PATH
+
+
+def predict(image):
+    print("begin loading model")
+    model = keras.models.load_model(MODEL_PATH)
+    print("end loading model")
+    print("begin prediction")
+    model_output = model.predict(np.array([image]))
+    print("end prediction")
+
+    model_output_2d = np.squeeze(model_output)
+    predicted_mask = (model_output_2d > 0.5).astype(int)
+
+    return predicted_mask
