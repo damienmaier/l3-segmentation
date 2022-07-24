583b4723e68c9b23743dea2a267b58d7fde733f1
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-07-24 15:57:13 +0200

    refactor, learning rate

--- src/main.py
+++ src/main.py
@@ -1,6 +1,3 @@
-from data_loading import get_test_set, get_train_set
-from model_training import train_best_model
+from model_training import get_best_model
 
-X_train, Y_train = get_train_set()
-
-train_best_model(X_train, Y_train)
+get_best_model()
