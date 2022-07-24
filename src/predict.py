c57f5d0e182b1e08e20dea893144880cf03911fc
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-07-24 13:48:55 +0200

    load model once instead of for each prediction

--- src/predict.py
+++ src/predict.py
@@ -3,11 +3,11 @@ import numpy as np
 
 from model_training import MODEL_PATH
 
+model = keras.models.load_model(MODEL_PATH)
+model.summary()
+
 
 def predict(image):
-    print("begin loading model")
-    model = keras.models.load_model(MODEL_PATH)
-    print("end loading model")
     print("begin prediction")
     model_output = model.predict(np.array([image]))
     print("end prediction")
