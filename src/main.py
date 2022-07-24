ac1d191a08843b63b363fd24e554f7e54926187e
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-07-24 13:50:03 +0200

    correct mistake that made train on test set istead of train set

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from data_loading import get_test_set
+from data_loading import get_test_set, get_train_set
 from model_training import train_best_model
 
-X_train, Y_train = get_test_set()
+X_train, Y_train = get_train_set()
 
 train_best_model(X_train, Y_train)
