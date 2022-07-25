fadfa05b3c30fc367e7ab5428ffe4cc91d7150e2
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-07-25 16:39:14 +0200

    add code for model performance evaluation

---
+++ src/model_evaluation.py
@@ -0,0 +1,44 @@
+import random
+import statistics
+
+import numpy as np
+import sklearn
+from matplotlib import pyplot as plt
+
+import dataset
+import utils.display_image
+from predict import TEST_SET_PREDICTIONS_PATH
+import pandas
+import seaborn
+
+
+def dice_coefficient(mask1, mask2):
+    return sklearn.metrics.f1_score(mask1.ravel(), mask2.ravel())
+
+
+def dice_coefficients(masks1, masks2):
+    return list(map(dice_coefficient, masks1, masks2))
+
+
+def average_dice_coefficient(masks1, masks2):
+    dice_coefficients_list = dice_coefficients(masks1, masks2)
+    return statistics.mean(dice_coefficients_list)
+
+
+def model_performance_summary():
+    images, true_masks = dataset.data_loading.get_test_set()
+    predicted_masks = np.load(TEST_SET_PREDICTIONS_PATH)
+    for image, true_mask, predicted_mask in random.sample(list(zip(images, true_masks, predicted_masks)), 20):
+        utils.display_image.display_ct_scan_image_and_two_masks(image=image, blue_mask=true_mask, red_mask=predicted_mask)
+
+    dice_coefficients_list = dice_coefficients(true_masks, predicted_masks)
+    seaborn.catplot(
+        data=(pandas.DataFrame({"dice coefficient": dice_coefficients_list})),
+        y="dice coefficient",
+        kind="box")
+    plt.show()
+
+    print(f"Average dice coefficient : {statistics.mean(dice_coefficients_list):.3f}")
+
+
+
