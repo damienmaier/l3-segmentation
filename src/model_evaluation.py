2db328f6990404f25132fa05a43e2e3984360b99
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-07-28 13:05:02 +0200

    rewrite dice to use tf

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,45 +1,58 @@
 import random
-import statistics
 
-import numpy as np
 import pandas
 import seaborn
+import tensorflow as tf
 from matplotlib import pyplot as plt
 
+import arch.custom_layers
 import utils.display_image
 
 
-def dice_coefficient(mask1, mask2):
-    assert(mask1.ndim == 2)
-    assert(mask2.ndim == 2)
+def dice_coefficient_between_two_masks(mask1: tf.Tensor, mask2: tf.Tensor):
+    mask1_bool = mask1 == 1
+    mask2_bool = mask2 == 1
 
-    intersection_size = np.count_nonzero(np.logical_and(mask1, mask2))
-    mask1_size = np.count_nonzero(mask1)
-    mask2_size = np.count_nonzero(mask2)
+    intersection_mask = tf.math.logical_and(mask1_bool, mask2_bool)
+    intersection_size = tf.math.count_nonzero(intersection_mask)
+
+    mask1_size = tf.math.count_nonzero(mask1)
+    mask2_size = tf.math.count_nonzero(mask2)
+
     return 2 * intersection_size / (mask1_size + mask2_size)
 
 
-def dice_coefficients(masks1, masks2):
-    return list(map(dice_coefficient, masks1, masks2))
+def dice_coefficients_between_multiple_pairs_of_masks(masks1: tf.Tensor, masks2: tf.Tensor):
+    def dice_coefficient_between_two_stacked_masks(tensor):
+        return dice_coefficient_between_two_masks(tensor[0], tensor[1])
+
+    stacked_masks = tf.stack([masks1, masks2], axis=1)
+    dice_coefficients = tf.vectorized_map(dice_coefficient_between_two_stacked_masks, stacked_masks)
+    return dice_coefficients
 
 
-def average_dice_coefficient(masks1, masks2):
-    dice_coefficients_list = dice_coefficients(masks1, masks2)
-    return statistics.mean(dice_coefficients_list)
+def average_dice_coefficient(masks1: tf.Tensor, masks2: tf.Tensor):
+    dice_coefficients = dice_coefficients_between_multiple_pairs_of_masks(masks1, masks2)
+    return tf.reduce_mean(dice_coefficients)
+
+
+def dice_metric_for_tf_model(true_masks: tf.Tensor, model_outputs: tf.Tensor):
+    predicted_masks = arch.custom_layers.RoundLayer()(model_outputs)
+    return dice_coefficients_between_multiple_pairs_of_masks(true_masks, predicted_masks)
 
 
 def model_performance_summary(images, true_masks, predicted_masks):
-    for image, true_mask, predicted_mask in random.sample(list(zip(images, true_masks, predicted_masks)), 10):
+    for image, true_mask, predicted_mask in random.sample(list(zip(images, true_masks, predicted_masks)), 2):
         utils.display_image.display_ct_scan_image(image)
         utils.display_image.display_ct_scan_image_and_mask(image, predicted_mask)
         utils.display_image.display_ct_scan_image_and_two_masks(image=image, blue_mask=true_mask,
                                                                 red_mask=predicted_mask)
 
-    dice_coefficients_list = dice_coefficients(true_masks, predicted_masks)
+    dice_coefficients_list = dice_coefficients_between_multiple_pairs_of_masks(true_masks, predicted_masks)
     seaborn.catplot(
         data=(pandas.DataFrame({"dice coefficient": dice_coefficients_list})),
         y="dice coefficient",
         kind="box")
     plt.show()
 
-    print(f"Average dice coefficient : {statistics.mean(dice_coefficients_list):.3f}")
+    print(f"Average dice coefficient : {average_dice_coefficient(true_masks, predicted_masks):.4f}")
