4edc439dd4f6cdb7456ff0c02946af9ee3472388
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-07-28 17:22:00 +0200

    use a validation set when training

--- src/model_training.py
+++ src/model_training.py
@@ -1,8 +1,11 @@
 import keras
+import keras_tuner
 import numpy as np
+import sklearn.model_selection
 import tensorflow as tf
-import arch.model_building.joachim
+
 import arch.custom_layers
+import arch.model_building.joachim
 import config
 import model_evaluation
 
@@ -15,12 +18,18 @@ def get_best_model(images: np.ndarray, masks: np.ndarray) -> keras.Model:
     return best_model_with_final_round_layer
 
 
-def _train_model(images: np.ndarray, masks: np.ndarray) -> keras.Model:
-    images_dataset = tf.data.Dataset.from_tensor_slices(images)
-    masks_dataset = tf.data.Dataset.from_tensor_slices(masks)
-    dataset = tf.data.Dataset.zip((images_dataset, masks_dataset))
+def _create_dataset_from_data(images: np.ndarray, masks: np.ndarray, batch_size: int) -> tf.data.Dataset:
+    base_dataset = tf.data.Dataset.from_tensor_slices((images, masks))
+    prepared_dataset = base_dataset.shuffle(len(images)).batch(batch_size).prefetch(buffer_size=1)
+    return prepared_dataset
 
-    batched_dataset = dataset.shuffle(len(images)).batch(config.TRAINING_BATCH_SIZE).prefetch(buffer_size=1)
+
+def _train_model(images: np.ndarray, masks: np.ndarray) -> keras.Model:
+    images_train, images_validation, masks_train, masks_validation = \
+        sklearn.model_selection.train_test_split(images, masks)
+
+    train_dataset = _create_dataset_from_data(images_train, masks_train, config.TRAINING_BATCH_SIZE)
+    validation_dataset = _create_dataset_from_data(images_validation, masks_validation, config.PREDICTION_BATCH_SIZE)
 
     model = arch.model_building.joachim.model_sma_detection()
 
@@ -31,8 +40,10 @@ def _train_model(images: np.ndarray, masks: np.ndarray) -> keras.Model:
     )
 
     model.fit(
-        x=batched_dataset,
+        x=train_dataset,
+        validation_data=validation_dataset,
         epochs=100
     )
 
     return model
+
