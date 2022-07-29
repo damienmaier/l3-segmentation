30ff8858208c3b09cd6374c9d0f3eb7dc777e981
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-07-30 00:44:52 +0200

    correction

--- src/architectures/__init__.py
+++ src/architectures/__init__.py
@@ -3,5 +3,5 @@ import architectures._deep_lab_v3
 
 architecture_builders = {
     "unet": _unet.model_sma_detection,
-    "deeplabv3": _deep_lab_v3
+    "deeplabv3": _deep_lab_v3.deep_lab_v3_plus
 }
