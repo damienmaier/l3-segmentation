4b1873557f6ceb175a8cce6f97ba860eed00e134
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-08-02 00:18:47 +0200

    preprocessing, data augmentation

--- src/model_training.py
+++ src/model_training.py
@@ -1,15 +1,14 @@
+import functools
+
 import keras
 import keras.layers
 import keras_tuner
-import sklearn.model_selection
 import tensorflow as tf
 
 import architectures
 import config
 import custom_layers
-import data.preloaded.load
 import model_evaluation
-import preprocessing
 import utils.functional
 
 
@@ -22,19 +21,19 @@ def build_model(hp: keras_tuner.HyperParameters):
 def train_model(hp: keras_tuner.HyperParameters, base_model: keras.Model,
                 train_dataset: tf.data.Dataset, validation_dataset: tf.data.Dataset,
                 *args, **kwargs):
-    use_weighted_loss = hp.Boolean("weighted loss", default=False)
-    train_dataset = _prepare_dataset_for_training(train_dataset,
-                                                  batch_size=config.TRAINING_BATCH_SIZE,
-                                                  add_pixel_weights=use_weighted_loss)
+    train_dataset = _prepare_dataset_for_training(train_dataset, batch_size=config.TRAINING_BATCH_SIZE,
+                                                  is_validation_dataset=False, hp=hp)
 
     if validation_dataset is not None:
-        validation_dataset = _prepare_dataset_for_training(validation_dataset,
-                                                           batch_size=config.PREDICTION_BATCH_SIZE,
-                                                           add_pixel_weights=use_weighted_loss)
+        validation_dataset = _prepare_dataset_for_training(validation_dataset, batch_size=config.TRAINING_BATCH_SIZE,
+                                                           is_validation_dataset=True, hp=hp)
 
-    preprocessing_model = preprocessing.get_preprocessing_model(train_dataset, hp)
     final_model = keras.Sequential()
-    final_model.add(preprocessing_model)
+    final_model.add(keras.Input(shape=(512, 512, 1)))
+    if hp.Boolean("clip preprocessing", default=True):
+        final_model.add(custom_layers.ClipLayer())
+    if hp.Boolean("data normalization", default=True):
+        final_model.add(custom_layers.normalization(train_dataset))
     final_model.add(base_model)
 
     learning_rate = hp.Float(
@@ -46,11 +45,9 @@ def train_model(hp: keras_tuner.HyperParameters, base_model: keras.Model,
     )
 
     def dice(true_masks: tf.Tensor, model_outputs: tf.Tensor):
-
-        predicted_masks = custom_layers.RoundLayer()(model_outputs)
-        predicted_masks_2d = tf.reshape(predicted_masks, shape=(-1, 512, 512))
+        predicted_masks = post_processing_model(model_outputs)
         true_masks_2d = tf.reshape(true_masks, shape=(-1, 512, 512))
-        return model_evaluation.dice_coefficients_between_multiple_pairs_of_masks(predicted_masks_2d, true_masks_2d)
+        return model_evaluation.dice_coefficients_between_multiple_pairs_of_masks(predicted_masks, true_masks_2d)
 
     final_model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
@@ -63,21 +60,43 @@ def train_model(hp: keras_tuner.HyperParameters, base_model: keras.Model,
     history = final_model.fit(
         x=train_dataset,
         validation_data=validation_dataset,
-        epochs=5,
         *args, **kwargs
     )
 
     return history
 
 
-def _prepare_dataset_for_training(dataset: tf.data.Dataset, batch_size: int,
-                                  add_pixel_weights: bool) -> tf.data.Dataset:
-    dataset = dataset.map(
-        lambda image, mask: (image, keras.layers.Reshape(target_shape=(512, 512, 1))(mask))
-    )
-    if add_pixel_weights:
+def _prepare_dataset_for_training(dataset: tf.data.Dataset, batch_size: int, is_validation_dataset: bool,
+                                  hp: keras_tuner.HyperParameters) -> tf.data.Dataset:
+    add_color_axis = functools.partial(tf.reshape, shape=(512, 512, 1))
+    dataset = dataset.map(utils.functional.function_on_pair(add_color_axis))
+
+    if not is_validation_dataset:
+        dataset = _perform_data_augmentation(dataset, hp)
+
+    if hp.Boolean("weighted loss", default=False):
         dataset = dataset.map(_add_pixel_weights)
+
     dataset = dataset.batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+    return dataset
+
+
+def _perform_data_augmentation(dataset: tf.data.Dataset, hp: keras_tuner.HyperParameters):
+    if hp.Boolean("horizontal flip", default=True):
+        def random_left_right_flip(image, mask):
+            seed = tf.random.uniform(shape=(2,), maxval=10000, dtype=tf.int32)
+            transformed_image = tf.image.stateless_random_flip_left_right(image, seed)
+            transformed_mask = tf.image.stateless_random_flip_left_right(mask, seed)
+            return transformed_image, transformed_mask
+
+        dataset = dataset.map(random_left_right_flip)
+
+    def gaussian_noise(image, mask):
+        gaussian_noise_standard_deviation = hp.Float("gaussian noise", min_value=0, max_value=50, default=25)
+        gaussian_noise_layer = keras.layers.GaussianNoise(gaussian_noise_standard_deviation)
+        return gaussian_noise_layer(image, training=True), mask
+
+    dataset = dataset.map(gaussian_noise)
     return dataset
 
 
@@ -93,3 +112,9 @@ def _add_pixel_weights(image: tf.Tensor, mask: tf.Tensor):
     pixel_weights = tf.gather(class_weights, indices=tf.cast(mask, tf.int32))
 
     return image, mask, pixel_weights
+
+
+post_processing_model = keras.Sequential()
+post_processing_model.add(keras.Input(shape=(512, 512, 1)))
+post_processing_model.add(custom_layers.RoundLayer())
+post_processing_model.add(keras.layers.Reshape(target_shape=(512, 512)))
