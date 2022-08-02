641177678a5220a23a54ce476cf56bed0267d435
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-08-02 22:59:46 +0200

    prepare main

--- src/main.py
+++ src/main.py
@@ -48,7 +48,7 @@ def evaluate_performance_of_predictions_on_test_set():
 # create_preloaded_dataset_from_original_dataset()
 
 # -------- Tune model --------
-explore_models()
+# explore_models()
 
 # -------- Train chosen model on train set --------
 # train_best_model()
@@ -58,3 +58,5 @@ explore_models()
 
 # -------- Visualize performance --------
 # evaluate_performance_of_predictions_on_test_set()
+
+model_exploration.train_default_model()
