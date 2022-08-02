ffa79e41a1785708982091efb57e21eb9b443a81
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-08-02 22:59:03 +0200

    add train default model function

--- src/model_exploration.py
+++ src/model_exploration.py
@@ -22,6 +22,13 @@ def explore_hyper_parameters():
     tuner.search(callbacks=[keras.callbacks.TensorBoard(rootdir.PROJECT_ROOT_PATH / "model tuning" / "tensorboard")])
 
 
+def train_default_model():
+    hyper_model = MyHyperModel()
+    hp = keras_tuner.HyperParameters()
+    model = hyper_model.build(hp)
+    hyper_model.fit(hp, model)
+
+
 class MyHyperModel(keras_tuner.HyperModel):
     def build(self, hp: keras_tuner.HyperParameters):
         keras.backend.clear_session()
@@ -30,7 +37,8 @@ class MyHyperModel(keras_tuner.HyperModel):
 
     def fit(self, hp: keras_tuner.HyperParameters, model: keras.Model, *args, **kwargs):
         train_validation_split_random_state = hp.Int("train val split", min_value=0, max_value=100000)
-        train_dataset, validation_dataset = data.preloaded.load.train_validation_tf_datasets(random_state=train_validation_split_random_state)
+        train_dataset, validation_dataset = data.preloaded.load.train_validation_tf_datasets(
+            random_state=train_validation_split_random_state)
         return model_training.train_model(
             hp=hp, base_model=model, train_dataset=train_dataset, validation_dataset=validation_dataset,
             *args, **kwargs)
