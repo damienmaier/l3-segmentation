e95b89aea73f52274a646f7e6fa4f4d9f9854ef4
Damien Maier <91182145+damienmaier@users.noreply.github.com>
2022-08-03 21:33:19 +0200

    add method to remove small areas, comments

--- src/utils/mask_processing.py
+++ src/utils/mask_processing.py
@@ -4,13 +4,10 @@ import math
 import numpy as np
 import scipy.spatial
 import skimage.measure
-
-# those values have been empirically found to give good results
-MAX_DISTANCE = 10
-MIN_RATIO = .1
+import skimage.morphology
 
 
-def remove_isolated_areas(mask):
+def remove_isolated_areas(mask, min_ratio, max_distance):
     labeled_mask, objects_count = skimage.measure.label(mask, return_num=True)
 
     pixels_counter = collections.Counter(labeled_mask.flat)
@@ -19,21 +16,22 @@ def remove_isolated_areas(mask):
     largest_object_id, largest_object_size = largest_objects[1]
     other_objects = largest_objects[2:]
 
-    # to accept an object, we give it the value largest_object_id
+    # to include an object in the final mask, we give it the value largest_object_id
 
-    # accept big objects
+    # find the main mask areas
+    # the objects whose size is close enough to the size of the largest object are part of the main mask area
     for object_id, object_size in other_objects:
-        if object_size >= MIN_RATIO * largest_object_size:
+        if object_size >= min_ratio * largest_object_size:
             labeled_mask[labeled_mask == object_id] = largest_object_id
 
-    # iteratively accept objects that are close enough to an already accepted object
+    # iteratively accept objects that are close enough to an already accepted area
     continue_adding_objects = True
     while continue_adding_objects:
         continue_adding_objects = False
         for object_id, _ in other_objects:
             distance_to_largest_object = _min_distance_between_two_objects(labeled_mask, largest_object_id,
                                                                            object_id)
-            if distance_to_largest_object <= MAX_DISTANCE:
+            if distance_to_largest_object <= max_distance:
                 labeled_mask[labeled_mask == object_id] = largest_object_id
                 continue_adding_objects = True
 
@@ -49,3 +47,10 @@ def _min_distance_between_two_objects(labeled_image, object1_id, object2_id):
 
     distances = scipy.spatial.KDTree(points_coordinates_object1).query(points_coordinates_object2, k=1)[0]
     return math.inf if len(distances) == 0 else min(distances)
+
+
+def remove_small_areas(mask: np.ndarray, max_pixel_count: int):
+    mask_bool = mask.astype(bool)
+    mask_bool_without_isolated_pixels = skimage.morphology.remove_small_objects(mask_bool, connectivity=2,
+                                                                                min_size=max_pixel_count + 1)
+    return mask_bool_without_isolated_pixels.astype(int)
